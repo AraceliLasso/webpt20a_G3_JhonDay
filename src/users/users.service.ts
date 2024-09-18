d2f2d41b607c3091ed115102c0a0b6484c6a5f20
PMoyanoAiraldi <pmoyano497@gmail.com>
2024-09-18 16:18:33 -0300

    Guardar cambios locales antes de la fusión con Ramagerman

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./users.entity";
 import { Repository } from "typeorm";
+import { UserWithAdminDto } from "./dto/admin-user.dto";
 
 @Injectable()
 export class UsersService{
