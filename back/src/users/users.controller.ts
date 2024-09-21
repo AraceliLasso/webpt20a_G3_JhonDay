62c30e024447ba6384d2bec975e351cda3ad779f
Araceli Lasso <lassoaraceli@gmail.com>
2024-09-21 10:24:40 -0300

    version estable de Paula

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,15 +1,19 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, NotFoundException, Param, ParseUUIDPipe, Post, Put, Query, Req, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UserWithAdminDto } from "./dto/admin-user.dto";
-import { ApiQuery, ApiSecurity } from "@nestjs/swagger";
+import { ApiQuery, ApiSecurity, ApiTags } from "@nestjs/swagger";
 import UserResponseDto from "./dto/response-user.dto";
 import { IsUUID } from "class-validator";
 import { updateUserDto } from "./dto/update-user.dto";
 import { User } from "./users.entity";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { AuthGuard } from "src/guard/auth.guard";
+import { RolesGuard } from "src/guard/roles.guard";
+import { Roles } from "src/decorators/roles.decorator";
 
 
+@ApiTags("Users")
 @Controller('users')
 export class UsersController{
     constructor(private readonly usersService: UsersService) {}
@@ -28,8 +32,8 @@ export class UsersController{
 
     @Get()  
     @HttpCode(HttpStatus.OK)
-    //@UseGuards(AuthGuard, RolesGuard)
-    //@Roles('admin')
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles('admin')
     @ApiSecurity('bearer')
     @ApiQuery({ name: 'page', required: false, description: 'Número de página', example: 1 })
     @ApiQuery({ name: 'limit', required: false, description: 'Cantidad de resultados por página', example: 5 })
@@ -39,9 +43,8 @@ export class UsersController{
     ): Promise<UserWithAdminDto[]>{ 
         return this.usersService.getUsers(page, limit);
     }
-
     @Get(':id')
-    //@UseGuards(AuthGuard)
+    @UseGuards(AuthGuard)
     @ApiSecurity('bearer')
     @HttpCode(HttpStatus.OK)
     async getUser(@Param('id', new ParseUUIDPipe()) id: string): Promise<UserResponseDto>{
@@ -56,16 +59,15 @@ export class UsersController{
     }
 
     @Put(':id')
-    //@UseGuards(AuthGuard)
+    @UseGuards(AuthGuard)
     @ApiSecurity('bearer')
     @HttpCode(HttpStatus.OK)
     async updateUsers(@Param('id') id: string, @Body() updateUser: updateUserDto): Promise<User>{
         const user = await this.usersService.updateUsers(id, updateUser) 
         return user;
     }
-
     @Delete(':id')
-    //@UseGuards(AuthGuard)
+    @UseGuards(AuthGuard)
     @ApiSecurity('bearer')
     @HttpCode(HttpStatus.OK)
     async deleteUsers(@Param('id') id: string): Promise<{id: string}>{
